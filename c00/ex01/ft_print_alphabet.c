34c6ce0389fbea5ce905edc1f822659ff57df06b
raphaelalvaro <100107054+raphaelalvaro@users.noreply.github.com>
2022-02-21 13:46:01 +1030

    Add files via upload

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralvaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/15 11:44:15 by ralvaro           #+#    #+#             */
+/*   Updated: 2022/01/20 16:36:08 by ralvaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char letter)
+{
+	write(1, &letter, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralvaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/15 12:35:18 by ralvaro           #+#    #+#             */
+/*   Updated: 2022/01/20 16:36:17 by ralvaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char letter)
+{
+	write(1, &letter, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter --;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
