3b75aca2fd5d657c2d031f0a46b205c59aaeb4d5
raphaelalvaro <100107054+raphaelalvaro@users.noreply.github.com>
2022-02-21 13:50:12 +1030

    Add files via upload

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralvaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 13:46:32 by ralvaro           #+#    #+#             */
+/*   Updated: 2022/01/31 16:37:34 by ralvaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	j;
+
+	j = 1;
+	while (j < argc)
+	{
+		ft_putstr(argv[j]);
+		j++;
+	}
+	return (0);
+}
---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralvaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 13:50:21 by ralvaro           #+#    #+#             */
+/*   Updated: 2022/01/31 16:42:46 by ralvaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	j;
+
+	if (argc >= 2)
+	{
+		j = argc - 1;
+		while (j > 0)
+		{
+			ft_putstr(argv[j]);
+			j--;
+		}
+	}
+	return (0);
+}
